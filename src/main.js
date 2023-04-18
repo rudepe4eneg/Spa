26134834ddb234a4e881af9342e68eb790dd112c
Daniil Zaytsev <daniil_zaytsev_02@mail.ru>
2023-04-18 14:59:10 +0300

    cardImgFix

--- src/main.js
+++ src/main.js
@@ -1,74 +1,118 @@
 import { el, setChildren } from 'redom';
-import Navigo from 'navigo';
+//import Navigo from 'navigo';
 
-const router = new Navigo('/');
+const div = el('div', { class: 'container' });
 
-function catalogList() {
-    const body = el('div', 'Loading...');
+const divLeft = el('div', { class: 'left' });
 
-    fetch('https://fakestoreapi.com/products').then(async (res) => {
-        const data = await res.json();
+const h1 = el('h1', { 
+    class: 'text-center' 
+}, 'Bank Card');
+const cardWrapper = el('div', { class: 'card-wrapper' });
 
-        const ul = el('ul');
+const cardBack = el('div', { class: 'card-back' });
 
-        setChildren(ul, data.map(product => el(
-            'li',
-            el('a', {
-                href: `/product/${product.id}`,
-                onclick(event) {
-                    event.preventDefault();
-                    router.navigate(event.target.getAttribute('href'));
-                }
-            }, product.title)
-        )));
 
-        body.innerHTML = '';
-        setChildren(body, ul);
-    });
+const cardBackLine = el('div', { class: 'card-back-line' });
+const inputCvv = el('input', { type: 'text', id: 'cvv' });
 
-    return el('div', [
-        el('h1', 'Product List'),
-        body,
-    ]);
-}
+const cardFront = el('div', { class: 'card-front' });
+const cardImg = el('img', { class: 'card-image', src: './svg/tinkoff.svg'});
+const cardLogo = el('img', { class: 'card-logo', src: './svg/visa.svg' });
+const cardNumber = el('input', { type: 'text', id: 'card-number' });
+const cardMonth = el('input', { type: 'text', id: 'card-to-month' });
+const cardYear = el('input', { type: 'text', id: 'card-to-year' });
 
-function catalogDetails(id) {
-    const body = el('div', 'Loading...');
+setChildren(cardFront, [
+    cardImg,
+    cardLogo,
+    cardNumber,cardMonth,
+    cardYear
+]);
+setChildren(cardBack, [
+    cardBackLine,
+    inputCvv
+])
+setChildren(cardWrapper, [
+    cardBack,
+    cardFront
+]);
+setChildren(divLeft, [
+    h1,
+    cardWrapper
+]);
+setChildren(div, divLeft);
+setChildren(window.document.body, div);
 
-    fetch(`https://fakestoreapi.com/products/${id}`).then(async (res) => {
-        const data = await res.json();
 
-        body.innerHTML = '';
+// const router = new Navigo('/');
 
-        setChildren(body, [
-            el('a', {
-                href: `/`,
-                onclick(event) {
-                    event.preventDefault();
-                    router.navigate(event.target.getAttribute('href'));
-                },
-            }, 'Back to list'),
-            el('h2', data.title),
-            el('p', data.description),
-            el('img', {
-                src: data.image,
-                alt: data.title,
-            }),
-        ])
-    });
+// function catalogList() {
+//     const body = el('div', 'Loading...');
 
-    return el('div', [
-        el('h1', `Product details ${id}`),
-        body,
-    ]);
-}
+//     fetch('https://fakestoreapi.com/products').then(async (res) => {
+//         const data = await res.json();
 
-router.on('/', () => {
-    setChildren(window.document.body, catalogList());
-});
+//         const ul = el('ul');
 
-router.on('/product/:id', ({ data: { id } }) => {
-    setChildren(window.document.body, catalogDetails(id));
-});
+//         setChildren(ul, data.map(product => el(
+//             'li',
+//             el('a', {
+//                 href: `/product/${product.id}`,
+//                 onclick(event) {
+//                     event.preventDefault();
+//                     router.navigate(event.target.getAttribute('href'));
+//                 }
+//             }, product.title)
+//         )));
 
-router.resolve();
+//         body.innerHTML = '';
+//         setChildren(body, ul);
+//     });
+
+//     return el('div', [
+//         el('h1', 'Product List'),
+//         body,
+//     ]);
+// }
+
+// function catalogDetails(id) {
+//     const body = el('div', 'Loading...');
+
+//     fetch(`https://fakestoreapi.com/products/${id}`).then(async (res) => {
+//         const data = await res.json();
+
+//         body.innerHTML = '';
+
+//         setChildren(body, [
+//             el('a', {
+//                 href: `/`,
+//                 onclick(event) {
+//                     event.preventDefault();
+//                     router.navigate(event.target.getAttribute('href'));
+//                 },
+//             }, 'Back to list'),
+//             el('h2', data.title),
+//             el('p', data.description),
+//             el('img', {
+//                 src: data.image,
+//                 alt: data.title,
+//             }),
+//         ])
+//     });
+
+//     return el('div', [
+//         el('h1', `Product details ${id}`),
+//         body,
+//     ]);
+// }
+
+// router.on('/', () => {
+//     setChildren(window.document.body, catalogList());
+// });
+
+// router.on('/product/:id', ({ data: { id } }) => {
+//     setChildren(window.document.body, catalogDetails(id));
+// });
+
+// router.resolve();
